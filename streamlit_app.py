3ee5e35448df9b1a9573a4f97d1521fbad1a4bb8
Junko8824 <kuromasa40614@gmail.com>
2024-12-01 11:48:29 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,47 @@
 # Import python packages
 import streamlit as st
 #ステートメントのインポート
-from snowflake.snowpark.functions import col, when_matched
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(":cup_with_straw: Pending Smoothie Orders! :cup_with_straw:")
+st.title("Customize Your Smoothie! :cup_with_straw:")
 st.write(
-    """Orders that need to filled"""
+    """Choose the fruits you want in your custom Smoothie!"""
 )
 
+name_on_order = st.text_input('Name on Smoothie:',)
+st.write('The name on your Smoothie will be:', name_on_order)
 
-#FRUIT_NAME列のみを表示
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')) #FRUIT_NAME列のみを表示
+#st.dataframe(data=my_dataframe, use_container_width=True)
 
-if my_dataframe:
-    editable_df = st.experimental_data_editor(my_dataframe)
-    submitted = st.button('Submit')
-    if submitted:
+#複数選択を許可する
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:'
+    ,my_dataframe
+    ,max_selections = 5
+    )
+
+#if ingredients_list:で何も選択していないときは[]を出さないようにする。
+if ingredients_list:
+    ingredients_string = '' #LIST を STRING に変換するには、FOR LOOP ブロックを追加
+    
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' ' #+=は変数にすでにあるものに追加するという意味
+
+    #st.write(ingredients_string)
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """', '"""+name_on_order+ """')"""
+
+    #st.write(my_insert_stmt)
+    #st.stop()
+    
+    time_to_insert = st.button('Submit Order')
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success("'Your Smoothie is ordered,'+name_on_order+'", icon="✅")
         
-        og_dataset = session.table("smoothies.public.orders")
-        edited_dataset = session.create_dataframe(editable_df)
-    
-        try:
-            og_dataset.merge(edited_dataset
-                        , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
-                        ,[when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
-                        )
-            st.success("Someone clicked the button.", icon="👍")
-        except:
-            st:write('Smothing wet wrong.')
-    
-else:
-    st.success('There are no pending orders right now.', icon = "👍")
